3b56f3c65be10f554e36ba25621e1bb756ac0bc5
hairibar <aitor.iribar@gmail.com>
2018-10-03 12:52:14 +0200

    Platform gameplay state creado

--- Source/States/boot.js
+++ Source/States/boot.js
@@ -5,7 +5,11 @@ var bootState = function(game) {
 bootState.prototype = {
 
     preload: function() {
-        
+        //Inicializa el sistema de físicas
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.time.desiredFps = 60;
+
+        game.physics.arcade.gravity.y = -250;   
     },
 
     create: function() {
--- Source/States/platformGameplay.js
+++ Source/States/platformGameplay.js
@@ -6,10 +6,15 @@ platformGameplayState.prototype = {
 
     preload: function() {
         alert("Entered platform gameplay state");
+
+        //Cargar sprites
+        game.load.image("personaje", "Assets/Sprites/TestCharacter");
+        game.load.image("suelo", "Assets/Sprites/TestGround");
     },
 
     create: function() {
-        
+        //Fondo
+        this.stage.backgroundColor = "#333333";
     },
 
     update: function() {
