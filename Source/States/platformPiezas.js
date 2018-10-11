26be730a9d5301fdb8a504edaded7fe61a7f38ee
CreepyS <43405227+CreepyS@users.noreply.github.com>
2018-10-11 18:15:44 +0200

    Añadido Platform Piezas

--- Source/States/platformPiezas.js
+++ Source/States/platformPiezas.js
@@ -4,41 +4,14 @@ var platformPiezasState = function(game) {
 platformPiezasState.prototype = {
 
     preload: function() {
-         //Cargar sprites
-         game.load.image("pieza", "Assets/Sprites/cuboPrueba.png");
+
     },
 
     create: function() {
-        //Fondo
-        this.stage.backgroundColor = "#2d2d2d";
-        
-        this.pieza = game.add.sprite(300, 50,"pieza");
-
-        
-        game.physics.enable(this.pieza, Phaser.Physics.ARCADE);
-
-        
-        downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-        leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-        rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 
     },
 
     update: function() {
         
-        this.pieza.y++;
-        if (downKey.isDown)
-        {
-            this.pieza.y++;
-        }
-
-        if (leftKey.isDown)
-        {
-            this.pieza.x--;
-        }
-        else if (rightKey.isDown)
-        {
-            this.pieza.x++;
-        }
     }
 }
