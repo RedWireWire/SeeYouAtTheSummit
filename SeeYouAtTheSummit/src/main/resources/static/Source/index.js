50b378afeb61e5e871ff8374105bcf017a3db0e2
CreepyS <43405227+CreepyS@users.noreply.github.com>
2018-11-20 17:45:52 +0100

    Algunos botones recortados y añadidos los estados al index

--- SeeYouAtTheSummit/src/main/resources/static/Source/index.js
+++ SeeYouAtTheSummit/src/main/resources/static/Source/index.js
@@ -10,5 +10,7 @@ game.state.add("mainMenuState", mainMenuState);
 game.state.add("matchMakingState", matchMakingState);
 game.state.add("localMultiplayerState", localMultiplayerState);
 game.state.add("onlineMultiplayerState", onlineMultiplayerState);
-  
+game.state.add("singlePlayerState", singlePlayerState);
+game.state.add("trainingState", trainingState);
+
 game.state.start('bootState');
